eca7cb999509b237d78b06092ae909096555b7c7
yasunari89 <yasunari89@gmail.com>
2020-04-14 02:38:06 +0900

    add time measure

--- chapter1/1-2.py
+++ chapter1/1-2.py
@@ -1,4 +1,10 @@
-def check_permutation(str1, str2):
+import sys
+sys.path.append('./..')
+from time_measure import time_measure
+
+
+@time_measure(ndigits=10)
+def check_permutation_by_sort(str1, str2):
     if len(str1) != len(str2):
         return False
     else:
@@ -12,7 +18,26 @@ def check_permutation(str1, str2):
         return True
 
 
+@time_measure(ndigits=10)
+def check_permutation_by_ascii_list(str1, str2):
+    if len(str1) != len(str2):
+        return False
+    else:
+        ascii_list = [0 for _ in range(128)]
+        for letter in str1:
+            ascii_list[ord(letter)] += 1
+        for letter in str2:
+            ascii_list[ord(letter)] -= 1
+            if ascii_list[ord(letter)] < 0:
+                return False
+            else:
+                pass 
+        return True
+
+
 if __name__ == "__main__":
+
+
     class Tester:
         def __init__(self):
             self.n = 0
@@ -29,12 +54,21 @@ if __name__ == "__main__":
         def tell(self, result, answer):
             self.result = result
             self.answer = answer
-            remarks = {True: 'OK!', False: 'Failed.'}
+            remarks = {True: 'Completed.', False: 'Failed.'}
             test_result = self.test()
             return 'TEST {}: {}'.format(self.n, remarks[test_result])
 
     t = Tester()
-    print(t.tell(check_permutation('aab', 'aba'), True))
-    print(t.tell(check_permutation('aabc ', 'cab a'), True))
-    print(t.tell(check_permutation('aaccb', 'abac'), False))
-    print(t.tell(check_permutation('aabgsss', 'sabsasu'), False))
+    print(t.tell(check_permutation_by_sort('aab', 'aba'), True))
+    print(t.tell(check_permutation_by_ascii_list('aab', 'aba'), True))
+    print(t.tell(check_permutation_by_sort('aabc ', 'cab a'), True))
+    print(t.tell(check_permutation_by_ascii_list('aabc ', 'cab a'), True))
+    print(t.tell(check_permutation_by_sort('aaccb', 'abac'), False))
+    print(t.tell(check_permutation_by_ascii_list('aaccb', 'abac'), False))
+    print(t.tell(check_permutation_by_sort('aabgsss', 'sabsasu'), False))
+    print(t.tell(check_permutation_by_ascii_list('aabgsss', 'sabsasu'), False))
+
+    str1 = 'a' * 10000
+    str2 = 'a' * 10000
+    print(t.tell(check_permutation_by_sort(str1, str2), True))
+    print(t.tell(check_permutation_by_ascii_list(str1, str2), True))
---
+++ time_measure.py
@@ -0,0 +1,23 @@
+import time
+from functools import wraps
+
+def time_measure(ndigits=2):
+    """Print execution time [sec] of function/method
+    - ndigits: precision after the decimal point
+    """
+    def outer_wrapper(func):
+        # @wraps: keep docstring of "func"
+        @wraps(func)
+        def inner_wrapper(*args, **kwargs):
+            start = time.time()
+            result = func(*args, **kwargs)
+            end = time.time()
+            print("runtime: {sec} [sec]".format(
+                sec=round(end-start, ndigits))
+            )
+            return result
+        return inner_wrapper
+    return outer_wrapper
+
+def a():
+    print('A')
