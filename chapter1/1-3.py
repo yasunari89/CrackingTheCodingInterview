4d43c9cd7bbe0f65c3df33c3c15b64f9122a8bba
yasunari89 <yasunari89@gmail.com>
2020-04-15 22:05:46 +0900

    add 1-3

---
+++ chapter1/1-3.py
@@ -0,0 +1,25 @@
+import sys 
+sys.path.append('./..')
+from tester import Tester
+from time_measure import time_measure
+
+
+def find_blanks(string, true_length):
+    blank_indexes = []
+    for i in range(true_length):
+        if string[i] == '':
+            blank_indexes.append(i)
+    return blank_indexes
+
+
+def replace_to_20(string, indexes):
+    r_indexes = [indexes[i] for i in range(len(indexes, 0, -1))]
+    for i in r_indexes:
+        temp = string[i+1:]
+        string[i] = '%20'
+        string = string + temp
+    return string
+
+
+def URLify(string, true_length):
+    return replace_to_20(string, find_blanks(string, true_length))
