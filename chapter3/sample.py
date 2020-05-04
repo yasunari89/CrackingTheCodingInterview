c0bf432710c68747784c757d10a849be27fcff08
yasunari89 <yasunari89@gmail.com>
2020-05-05 00:05:01 +0900

    add QueueByStacks

--- chapter3/sample.py
+++ chapter3/sample.py
@@ -1,8 +1,14 @@
 from node import Node
 from stack import Stack
+from queue import Queue
+from queue_by_stacks import QueueByStacks
 
-s = Stack()
-s.push(Node(3))
-s.push(Node(1))
-s.push(Node(5))
-print(s)
+q = QueueByStacks()
+q.add(Node(3))
+q.add(Node(4))
+q.add(Node(5))
+print(q)
+q.remove()
+print(q)
+q.remove()
+print(q)
--- chapter3/stack.py
+++ chapter3/stack.py
@@ -5,27 +5,43 @@ class Stack:
         self.top = None
     
     def __iter__(self):
-        node = self.top 
-        yield node
-        while node.next:
-            yield node.next
-            node = node.next
+        if self.is_empty():
+            yield None
+        else:
+            node = self.top 
+            yield node
+            while node.next:
+                yield node.next
+                node = node.next
     
     def __repr__(self):
+        if self.is_empty():
+            return 'None'
         nodes = []
         for node in self:
             nodes.append(node)
-        return '(top) ' + ','.join([str(node.data) for node in nodes]) + ' (bottom)'
+        return ' -> '.join([str(node.data) for node in nodes])
 
     def push(self, node):
-        if self.top == None:
+        if self.is_empty():
             self.top = node 
         else:
             node.next = self.top 
             self.top = node
 
     def pop(self):
-        item = self.top
-        self.top = self.top.next
+        if self.is_empty():
+            self.top = None
+            item = None
+        else:
+            item = Node(self.top.data)
+            self.top = self.top.next
         return item
     
+    def is_empty(self):
+        if self.top:
+            return False
+        return True
+    
+    def peek(self):
+        return self.top
