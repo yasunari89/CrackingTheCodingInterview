3ee53e46c6b74f2f35495df8617ea6bcd93f7888
yasunari89 <yasunari89@gmail.com>
2020-05-08 02:41:07 +0900

    update dfs

--- chapter4/dfs.py
+++ chapter4/dfs.py
@@ -1,25 +1,12 @@
 from graph_node import GraphNode
 
 class DFS:
-    def search(self, root: GraphNode):
-        if root == None: return
+    def search(self, root: GraphNode, value):
+        if root == None: return False
         root.marked = True
-        print(root)
+        if root.data == value: return True
         for child_node in root.children:
             if child_node.marked == False:
-                self.search(child_node)
-
-
-root = GraphNode(0)
-graph_node1 = GraphNode(1)
-graph_node2 = GraphNode(2)
-graph_node3 = GraphNode(3)
-root.children.append(graph_node1)
-root.children.append(graph_node2)
-root.children.append(graph_node3)
-graph_node1.children.append(GraphNode(4))
-graph_node1.children.append(GraphNode(5))
-graph_node1.children.append(GraphNode(6))
-
-dfs = DFS()
-dfs.search(root)
+                if self.search(child_node, value):
+                    return True
+        return False
