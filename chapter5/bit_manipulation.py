5e1db540e154bbefc75b845f89f8d911af27bac5
yasunari89 <yasunari89@gmail.com>
2020-05-08 03:23:16 +0900

    add bit manipulation

---
+++ chapter5/bit_manipulation.py
@@ -0,0 +1,23 @@
+class BitManipulation:
+    def get_bit(self, num: int, index: int) -> bool:
+        return num & (num << index) != 0
+    
+    def set_bit(self, num: int, index: int) -> int:
+        return num | (1 << index)
+    
+    def clear_bit(self, num: int, index: int) -> int:
+        mask = -(1 << index)
+        return num & mask 
+    
+    def clear_bits_from_top(self, num: int, index: int) -> int:
+        mask = (1 << index) - 1
+        return num & mask
+    
+    def clear_bits_from_bottom(self, num: int, index: int) -> int:
+        mask = -1 << (index + 1)
+        return num & mask
+    
+    def update_bit(self, num: int, index: int, bit_is_one: bool) -> int:
+        value = bit_is_one if bit_is_one else 0
+        mask = -(1 << index)
+        return (num & mask) | (value << index)
