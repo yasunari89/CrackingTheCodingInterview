447a2a8911a910e4797d5b9ce65681d111063cfd
yasunari89 <yasunari89@gmail.com>
2020-05-11 22:49:48 +0900

    add magic index

---
+++ questions/chapter8/magic_index.py
@@ -0,0 +1,24 @@
+from typing import List
+import time
+
+class MagicIndex:
+    def magic_index(self, x: List[int]) -> int:
+        n = len(x) - 1
+        start = 0
+        end = n
+        while start + 1 != end:
+            time.sleep(1)
+            i = self.half_index(start, end)
+            if x[i] == i:
+                start = i
+            else:
+                end = i  
+        return start          
+
+    def half_index(self, start: int, end: int) -> int:
+        n = end - start + 1
+        if n % 2:
+            rest = int(n/2)
+        else:
+            rest = int(n/2 - 1)
+        return start + rest
